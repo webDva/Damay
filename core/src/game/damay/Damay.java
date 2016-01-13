f11c8750dfd47672779c5792e71e8207743d31fb
Easterling <windows146@users.noreply.github.com>
2016-01-13 07:42:17 -0500

    PlayScreen

--- core/src/game/damay/Damay.java
+++ core/src/game/damay/Damay.java
@@ -1,27 +1,24 @@
 package game.damay;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Damay extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Damay extends Game {
+	private SpriteBatch batch;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new PlayScreen(batch));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
 	}
 }
---
+++ core/src/game/damay/PlayScreen.java
@@ -0,0 +1,66 @@
+package game.damay;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class PlayScreen implements Screen {
+
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private FitViewport viewport;
+
+	private Stage stage;
+	private Skin skin;
+
+	public PlayScreen(SpriteBatch batch) {
+		this.batch = batch;
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		stage.dispose();
+	}
+
+}
