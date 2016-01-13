c2030146e5ae32cd009ceae6dfd0f36f76a27b25
Easterling <windows146@users.noreply.github.com>
2016-01-13 07:34:05 -0500

    init

---
+++ desktop/src/game/damay/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.damay.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.damay.Damay;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Damay(), config);
+	}
+}
