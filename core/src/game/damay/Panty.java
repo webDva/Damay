11c2fd5e9a8bc06d126e3a11b478be1623eec423
Easterling <windows146@users.noreply.github.com>
2016-01-13 09:02:27 -0500

    this is easier than i expected. added successful inputlistener Scene2D rocks!

--- core/src/game/damay/Panty.java
+++ core/src/game/damay/Panty.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 
 public class Panty extends Actor {
 	public Texture texture;
@@ -13,6 +15,20 @@ public class Panty extends Actor {
 	public Panty(Texture texture, Vector2 position) {
 		this.texture = texture;
 		this.positon = position;
+		setBounds(position.x, position.y, texture.getWidth(), texture.getHeight());
+
+		addListener(new InputListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("down");
+				return true;
+			}
+
+			@Override
+			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("up");
+			}
+		});
 	}
 
 	@Override
